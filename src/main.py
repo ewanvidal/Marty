f95705be8ba81e771bc894d3e8b9de62cdcaf3fd
jonitreak <66328347+jonitreak@users.noreply.github.com>
2024-05-13 16:47:41 +0200

    fonction connection

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-import martypy 
-connectionWifi=True
-if (connectionWifi):
-    my_marty = martypy.Marty("wifi", "192.168.0.5")
-else:
-    my_marty = martypy.Marty("USB")
+import martypy
+def connection(connectionWifi=True, ip_adress="192.168.0.100"):
+    if connectionWifi:
+        my_marty = martypy.Marty("wifi", ip_adress)
+    else:
+        my_marty = martypy.Marty("USB")

b1bca29ee01265fadf47965062b1bce4f2c2e5ca
jonitreak <66328347+jonitreak@users.noreply.github.com>
2024-05-13 16:53:01 +0200

    mdoficiation fonction connection gestion d'erreur

--- src/main.py
+++ src/main.py
@@ -1,6 +1,17 @@
 import martypy
 def connection(connectionWifi=True, ip_adress="192.168.0.100"):
     if connectionWifi:
-        my_marty = martypy.Marty("wifi", ip_adress)
+        try:
+            my_marty = martypy.Marty("wifi", ip_adress)
+            return True
+        except: 
+            print("connection wifi impossible")
+            return False
     else:
-        my_marty = martypy.Marty("USB")
+        try:
+            my_marty = martypy.Marty("USB")
+            return True
+        except: 
+            print("connection USB impossible")
+            return False
+        

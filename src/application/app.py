28fe2d489feede98f087b6db40efa7fd153175d2
jonitreak <66328347+jonitreak@users.noreply.github.com>
2024-05-15 10:46:50 +0200

    création fichier connexion.py

---
+++ src/application/app.py
@@ -0,0 +1,17 @@
+import martypy
+def connexion(connexionWifi=True, ip_adress="192.168.0.100"):
+    if connexionWifi:
+        try:
+            my_marty = martypy.Marty("wifi", ip_adress)
+            return True
+        except: 
+            print("connexion wifi impossible")
+            return False
+    else:
+        try:
+            my_marty = martypy.Marty("USB")
+            return True
+        except: 
+            print("connexion USB impossible")
+            return False
+
--- src/main.py
+++ src/main.py
@@ -1,17 +1,1 @@
-import martypy
-def connexion(connexionWifi=True, ip_adress="192.168.0.100"):
-    if connexionWifi:
-        try:
-            my_marty = martypy.Marty("wifi", ip_adress)
-            return True
-        except: 
-            print("connexion wifi impossible")
-            return False
-    else:
-        try:
-            my_marty = martypy.Marty("USB")
-            return True
-        except: 
-            print("connexion USB impossible")
-            return False
-
+#main

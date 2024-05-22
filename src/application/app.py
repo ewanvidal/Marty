632938638ffbabbe244a3bae4b958299cc76be4a
jonitreak <66328347+jonitreak@users.noreply.github.com>
2024-05-22 11:11:00 +0200

    deplacement actualisé

--- src/application/app.py
+++ src/application/app.py
@@ -1,24 +1,11 @@
 from martypy import Marty
-def avance(distance,my_marty):
-    my_marty.walk(distance,'auto',0,25,1500,None)
-
-def connexion(connexionWifi=True, ip_adress="192.168.0.100"):
-    if connexionWifi:
-        try:
-            my_marty = Marty("wifi", ip_adress)
-            return (True,my_marty)
-        except: 
-            print("connexion wifi impossible")
-            return (False,None)
-    else:
-        try:
-            my_marty = Marty("USB")
-            return (True,my_marty)
-        except: 
-            print("connexion USB impossible")
-            return (False,None)
-        
-
-(connecter,my_marty)=connexion(True,"192.168.0.100")
-if(connecter):print("le robot est connecter")
-avance(5,my_marty)
+from connexion import connexion
+from deplacement import avance,rotate,recule
+def main():
+    
+    (connecter,my_marty)=connexion(True)
+    if(connecter):print("le robot est connecté")
+    avance(5,my_marty)
+    recule(5,my_marty)
+    rotate(117,'right',my_marty)
+main()
--- src/application/deplacement.py
+++ src/application/deplacement.py
@@ -1,3 +1,18 @@
 from martypy import Marty
 def avance(distance,my_marty):
+    my_marty.stand_straight()
     my_marty.walk(distance,'auto',0,25,1500,None)
+def rotate(angle,my_marty):
+    if(angle>0):
+        while(angle>20):
+            my_marty.walk(1,'auto',20)
+            angle=angle-20
+        my_marty.walk(1,'auto',angle)
+    else :
+        while(angle<-20):
+            my_marty.walk(1,'auto',-20)
+            angle=angle+20
+        my_marty.walk(1,'auto',angle)
+def recule(distance,my_marty):
+    rotate(180,my_marty)
+    avance(distance,my_marty)

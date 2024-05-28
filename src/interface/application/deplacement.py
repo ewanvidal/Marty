a4ba36fada58ea1750d94d088a01c17cafa605ba
ewanvidal <ewan@yse.fr>
2024-05-28 13:23:08 +0200

    Prise en compte des couleurs et dépalcements en conséquences

--- src/interface/application/deplacement.py
+++ src/interface/application/deplacement.py
@@ -1,4 +1,6 @@
 from martypy import Marty
+from sensors import *
+
 def avance(my_marty,distance):
     my_marty.stand_straight()
     for i in range(distance):
@@ -21,9 +23,8 @@ def rotate(my_marty,angle):
             my_marty.walk(0,'auto',-15)
             angle=angle+15
         my_marty.walk(0,'auto',angle)
-def retourner(my_marty,distance):
+def retourner(my_marty):
     rotate(my_marty,180)
-    avance(my_marty,distance)
 def recule(my_marty,distance):
     my_marty.walk(distance,'auto',0,-25,1500,None)
 
@@ -36,9 +37,29 @@ def deplacement_couleur(couleur):
         if (couleur=="purple"):
             return "right"
         if (couleur=="yellow"):
-            return "backward"
+            return "backwards"
         if (couleur=="red"):
             return "center"
-        
+    else :
+        print("Couleur non valide")
+
+def movementColorLab(my_marty):
+    color = getColorReading(my_marty)
+    movement = deplacement_couleur(color)
+    if movement == "forward" :
+        avance(my_marty,3)
+        return False
+    elif movement == "left" :
+        my_marty.sidestep('left', 3)
+        return False
+    elif movement == "right" :
+        my_marty.sidestep('right', 3)
+        return False
+    elif movement == "backwards" :
+        retourner(my_marty)
+        avance(my_marty,3)
+        return False
+    else :
+        return True
 
         

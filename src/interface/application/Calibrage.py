a7a6967f202fe3a24f015589df721dad40f21906
jonitreak <66328347+jonitreak@users.noreply.github.com>
2024-06-05 08:41:40 +0200

    Création Calibrage.txt et Calibrage.py

---
+++ src/interface/application/Calibrage.py
@@ -0,0 +1,16 @@
+import os
+
+premiereFois=True
+dicoCouleur={}
+
+def getCouleurCalibrage(color):
+    if (premiereFois):
+        dossier = os.path.dirname(__file__)
+        chemin_fichier = os.path.join(dossier, "Calibrage.txt")
+        fichier=open(chemin_fichier,"r")
+
+        for ligne in fichier: 
+            ligneSplit=ligne.split()
+            
+
+getCouleurCalibrage("")
